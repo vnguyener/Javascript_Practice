3b9eba3f740c7cbff992e059bf4aecec6dac16b2
vnguyener <its.vnguyen@gmail.com>
2016-06-14 22:46:16 -0500

    string match auto complete

--- Other/autocomplete.js
+++ Other/autocomplete.js
@@ -16,3 +16,26 @@ function changeInput(value) {
     var results = findPeople(value);
     document.getElementById("result").innerHTML = results;
 }
+
+
+// on keyup we grab the input
+// pass it back to our filter or db query from an ajax
+// do some sql like from where column name is like %input%
+
+
+let substringAutoComplete = (string, array) => {
+  var match = [];
+  array.forEach(function(value) {
+    if (value.includes(string)) {
+      match.push(value);
+    }
+  })
+  return match;  
+}
+
+let onInputChange = (value) => {
+  let arryay = ['vu', 'sujita', 'appa', 'apples'];
+  let results = substringAutoComplete(value, arryay);
+  console.log(results);
+  document.getElementById("result").innerHTML = results;
+}
